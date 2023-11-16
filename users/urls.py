5a2b05af241695187804993405977b22ee603150
mask-kakazey <63161131+mask-kakazey@users.noreply.github.com>
2023-11-16 23:58:28 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+    path('', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
     path('profile/', views.user_profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('logout/', views.custom_logout, name='logout'),
